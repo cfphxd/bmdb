24713acf84750547b132bda9c9885999440285e3
Helge <helge.dzierzon@plantandfood.co.nz>
2014-02-21 01:44:04 +0400

    added the option to run a shell from runserver.

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+shellcommand="shell"
+
 listcontains() {
   for word in $1; do
     [[ $word = $2 ]] && return 0
@@ -10,15 +12,17 @@ listcontains() {
 sudoportlist="80 443 8000"
 
 
+echo "Got ./runserver.sh $1 "
+
 if [ -z "$1" ]
 then
-	echo "Usage: ./runserver.sh port"
+	echo "Usage: ./runserver.sh port|shell"
+elif [ "$1" == "$shellcommand" ];
+then
+    ./manage.py shell
 elif listcontains "$sudoportlist" "$1" 
 then
 	sudo ./manage.py runserver 103.18.59.118:$1
-elif [ "$1" -eq 'shell' ]; 
-then
-    ./manage.py shell
 else
 	./manage.py runserver localhost:$1
 fi
